93d3261750c31dd805f8c77ae151c5e2c44db421
Julia <berrylily.0709@gmail.com>
2017-08-18 11:21:50 -0700

    Create Assignment2 project

---
+++ Assignment/Assignment2/Assignment2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment2
+//
+//  Created by Julia on 2017/08/18.
+//  © 2017 Julia
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
