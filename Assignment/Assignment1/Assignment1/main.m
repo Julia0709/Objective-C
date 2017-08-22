230e0bb6467ef9d6f060fa204b4098e766cc0ba8
Julia <berrylily.0709@gmail.com>
2017-08-21 20:20:57 -0700

    Update Assignment1 solution

--- Assignment/Assignment1/Assignment1/main.m
+++ Assignment/Assignment1/Assignment1/main.m
@@ -33,20 +33,51 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         NSString *askInputLowercase = @"Type something in lowercase";
-        NSString *aslInputUppercase = @"Type something in UPPERCASE";
+        NSString *askInputUppercase = @"Type something in UPPERCASE";
+        NSString *askInputNumber = @"Type some number";
+        NSString *askInputSentence = @"Type a sentence";
+        NSString *askInputQuestion = @"Type ? or ! at the end of your sentence";
+        NSString *askInputSpace =@"Type something with spaces";
 
         NSString *input = [[NSString alloc] init];
 
         while (1) {
+
             // 1. To Uppercase
             input = getUserInput(MAX, askInputLowercase);
             NSString *upper = [input uppercaseString];
             NSLog(@"%@ \n", upper);
             
             // 2. To Lowercase
-            input = getUserInput(MAX, aslInputUppercase);
+            input = getUserInput(MAX, askInputUppercase);
             NSString *lower = [input lowercaseString];
             NSLog(@"%@ \n", lower);
+            
+            // 3. Numberize
+            input = getUserInput(MAX, askInputNumber);
+            NSNumber *number = [[[NSNumberFormatter alloc] init] numberFromString: input];
+            if (number != nil) {
+                NSLog(@"%ld", [number integerValue]);
+            } else {
+                NSLog(@"%@", @"Can't be converted to integer.");
+            }
+            
+            // 4. Canadianize
+            input = getUserInput(MAX, askInputSentence);
+            NSLog(@"%@", [input stringByAppendingString:@", eh?"]);
+            
+            // 5. Respond
+            input = getUserInput(MAX, askInputQuestion);
+            if ([input characterAtIndex: [input length] - 1] == '?') {
+                NSLog(@"%@", @"I don't know");
+            } else if ([input characterAtIndex: [input length] - 1] == '!') {
+                NSLog(@"%@", @"Whoa, calm down!");
+            }
+            
+            // 6. De-Space
+            input = getUserInput(MAX, askInputSpace);
+            NSLog(@"%@", [input stringByReplacingOccurrencesOfString:@" " withString:@"-"]);
+            
         }
         
     }
