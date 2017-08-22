5847aa3d8b2bf9340f9d2562ffb3255c166620a0
Julia <berrylily.0709@gmail.com>
2017-08-22 09:51:23 -0700

    Create Assignment4  project

---
+++ Assignment/Assignment4/Assignment4/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment4
+//
+//  Created by Julia on 2017/08/22.
+//  Copyright © 2017 Julia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
