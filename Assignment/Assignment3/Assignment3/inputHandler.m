6946aa35d5721ea0d5830e86f06b14623f9307bd
Julia <berrylily.0709@gmail.com>
2017-08-21 20:26:40 -0700

    Create Assignment3 project

---
+++ Assignment/Assignment3/Assignment3/inputHandler.m
@@ -0,0 +1,20 @@
+//
+//  inputHandler.m
+//  Assignment3
+//
+//  Created by Julia on 2017/08/21.
+//  © 2017 Julia
+//
+
+#import <Foundation/Foundation.h>
+
+@implementation InputHandler
+
++ (NSString *) getUserInput {
+    char myStr[256];
+    fgets(myStr, 256, stdin);
+    NSString *input = [[NSString stringWithCString: myStr encoding: NSUTF8StringEncoding] stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    return input;
+}
+
+@end
---
+++ Assignment/Assignment3/Assignment3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Assignment3
+//
+//  Created by Julia on 2017/08/21.
+//  © 2017 Julia
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        NSLog(@"Math quiz!");
+
+    }
+    return 0;
+}
