9a9f5fa9690b3c7b24229e67189c6ce8da86e043
Julia <berrylily.0709@gmail.com>
2017-08-25 11:16:59 -0700

    Create Assignment13 project

---
+++ Assignment/Assignment13/Assignment13/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment13
+//
+//  Created by Julia on 2017/08/25.
+//  © 2017 Julia
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
